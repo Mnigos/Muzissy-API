bd0c29867840364f9393150de5cf29814c13ee05
MoneyIgos <makowskiigor@gmail.com>
2021-02-05 14:51:28 +0100

    style(server.tx): add mising comma

--- src/server.ts
+++ src/server.ts
@@ -12,7 +12,7 @@ mongoose.set('useFindAndModify', false);
 mongoose
   .connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
   })
   .then(() => {
     console.log('Mongoose is connected');
