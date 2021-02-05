a4a500621d3d569bc126dd71f6d06c6fc97f9013
MoneyIgos <makowskiigor@gmail.com>
2021-02-05 22:32:08 +0100

    refactor(login.ts): replace requireObjectKeysType function with express-validator

--- src/routes/auth/login.ts
+++ src/routes/auth/login.ts
@@ -1,4 +1,5 @@
 import { Request, Response, Router } from 'express';
+import { body, validationResult } from 'express-validator';
 import jwt from 'jsonwebtoken';
 import bcrypt from 'bcrypt';
 import User from '../../models/user';
@@ -6,32 +7,37 @@ import { requireObjectKeysType } from '../../validation';
 
 const router = Router();
 
-router.post('/login', async (req: Request, res: Response) => {
-  try {
-    if (!requireObjectKeysType(req.body, ['name', 'pass'], 'string'))
-      return res
-        .status(400)
-        .send({ e: 'both name and pass are required in body' });
+router.post(
+  '/login',
+  body('name').isString(),
+  body('pass').isString(),
+  async (req: Request, res: Response) => {
+    try {
+      if (!requireObjectKeysType(req.body, ['name', 'pass'], 'string'))
+        return res
+          .status(400)
+          .send({ e: 'both name and pass are required in body' });
 
-    const { name, pass } = req.body;
-    const foundedUser = await User.findOne({ name });
+      const { name, pass } = req.body;
+      const foundedUser = await User.findOne({ name });
 
-    if (!foundedUser) return res.status(400).send({ e: 'userNotFound' });
+      if (!foundedUser) return res.status(400).send({ e: 'userNotFound' });
 
-    const isPasswordCorrect = bcrypt.compareSync(pass, foundedUser.pass);
+      const isPasswordCorrect = bcrypt.compareSync(pass, foundedUser.pass);
 
-    if (!isPasswordCorrect)
-      return res.status(401).send({ e: 'password Incorrect' });
+      if (!isPasswordCorrect)
+        return res.status(401).send({ e: 'password Incorrect' });
 
-    const token = jwt.sign(
-      { user: foundedUser.name, perms: foundedUser.perms },
-      'privateKey'
-    );
+      const token = jwt.sign(
+        { user: foundedUser.name, perms: foundedUser.perms },
+        'privateKey'
+      );
 
-    res.send({ token, perms: foundedUser.perms });
-  } catch (e) {
-    res.status(500).send({ e });
+      res.send({ token, perms: foundedUser.perms });
+    } catch (e) {
+      res.status(500).send({ e });
+    }
   }
-});
+);
 
 export default router;
