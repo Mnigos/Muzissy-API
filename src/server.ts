c3387b6ee3dcc46d72ba2d36c7315eaf0c143c89
Kamil Pawlaczyk <prgmr.kamil.pawlaczyk@gmail.com>
2021-02-05 13:41:59 +0100

    feat(server.ts): configure a connection to the database

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,22 @@
+import mongoose from 'mongoose';
+import assert from 'assert';
 import app from './app';
 
-app.listen(process.env.PORT || 8080, () => {
-  console.log(`Listening on ${process.env.PORT}`);
+require('dotenv').config();
+
+['MONGO_URI'].forEach(variable => {
+  assert(process.env[variable], `proces.env${variable} is undefined!`);
 });
+mongoose.set('useFindAndModify', false);
+
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(() => {
+    console.log('Mongoose is connected');
+    app.listen(process.env.PORT || 8080, () => {
+      console.log(`Listening on ${process.env.PORT}`);
+    });
+  });
