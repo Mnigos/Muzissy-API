6cfd3211a485c28f1a27c770a87dcf815c0142e9
MoneyIgos <makowskiigor@gmail.com>
2021-02-05 22:32:54 +0100

    chore: delete unused validation functions

--- src/validation.ts
+++
@@ -1,8 +0,0 @@
-// In future there will be another functions
-
-// eslint-disable-next-line import/prefer-default-export
-export const requireObjectKeysType = (
-  obj: any,
-  keys: string[],
-  expectedType: string = 'string'
-): boolean => keys.every(key => typeof obj[key] === expectedType);
