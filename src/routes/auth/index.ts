23fb6ef11799c8f70cbc6e2490cc27fc37918c77
rysiek21 <rysiek21@users.noreply.github.com>
2021-02-06 14:00:58 +0100

    chore(router): add register endpoint to router

--- src/routes/auth/index.ts
+++ src/routes/auth/index.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 import loginRoute from './login';
+import registerRoute from './register';
 
 const router = Router();
 
 router.use('/', loginRoute);
+router.use('/', registerRoute);
 
 export default router;
