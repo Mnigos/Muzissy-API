4ada9f8cc2c7f0afd3b9330c22419011d8d77cc1
MoneyIgos <makowskiigor@gmail.com>
2021-02-07 14:57:50 +0100

    feat(quiz): add quiz endpoint

---
+++ src/routes/quiz/index.ts
@@ -0,0 +1,32 @@
+import { Request, Response, Router } from 'express';
+import { body, validationResult } from 'express-validator';
+import jwt from 'jsonwebtoken';
+
+const router = Router();
+
+router.post(
+  '/quiz/:id',
+  body('accesToken').isString(),
+  async (req: Request, res: Response) => {
+    try {
+      const { accessToken } = req.body;
+
+      const err = validationResult(req);
+      const validToken = accessToken.toMatch(
+        /^([a-zA-Z0-9-_.]+\.){2}[a-zA-Z0-9-_.]+$/i
+      );
+
+      if (!err.isEmpty())
+        return res.status(400).send({ err: 'token is required' });
+
+      if (!validToken) return res.status(400).send({ err: 'token is invalid' });
+
+      if (!jwt.verify(accessToken, process.env.TOKEN_SECRET))
+        return res.status(401).send({ err: 'bad token' });
+    } catch (e) {
+      res.status(500).send({ e });
+    }
+  }
+);
+
+export default router;
