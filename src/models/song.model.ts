a255b420a61535697660a64d0725ca5d5b109c7c
MoneyIgos <makowskiigor@gmail.com>
2021-02-07 12:40:16 +0100

    chore(song.model): add file path to schema

--- src/models/song.model.ts
+++ src/models/song.model.ts
@@ -4,6 +4,7 @@ export interface ISong extends Document {
   name: string;
   band: string;
   img: string;
+  file: string;
   genre: string;
 }
 
@@ -11,6 +12,7 @@ const songSchema = new Schema({
   name: String,
   band: String,
   img: String,
+  file: String,
   genre: String,
 });
 
