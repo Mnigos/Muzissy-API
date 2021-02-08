740d21877c1ff87e3834842239fd9e7247b121f6
MoneyIgos <makowskiigor@gmail.com>
2021-02-08 11:20:38 +0100

    feat(quiz): sending quiz songs

--- src/routes/quiz/index.ts
+++ src/routes/quiz/index.ts
@@ -10,6 +10,30 @@ router.post(
   async (req: Request, res: Response) => {
     try {
       const playlist = await Playlist.findOne({ _id: req.params.id });
+      const { songs } = playlist;
+
+      const randomNumber = () => Math.floor(Math.random() * songs.length);
+
+      const rightSong = songs[randomNumber()];
+
+      res.status(200).send({
+        songs: [
+          {
+            name: rightSong.name,
+            img: rightSong.img,
+            file: rightSong.file,
+          },
+          {
+            name: songs[randomNumber()].name,
+          },
+          {
+            name: songs[randomNumber()].name,
+          },
+          {
+            name: songs[randomNumber()].name,
+          },
+        ],
+      });
     } catch (err) {
       res.status(500).send({ err });
     }
