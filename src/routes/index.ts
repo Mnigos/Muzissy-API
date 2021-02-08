3dc3f8bfb82642b65497ab86204496e1d47b1974
rysiek21 <rysiek21@users.noreply.github.com>
2021-02-08 09:32:40 +0100

    chore(index): add admin route

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 import authRoutes from './auth';
+import adminRoutes from './admin';
 
 const router = Router();
 
 router.use('/auth', authRoutes);
+router.use('/admin', adminRoutes);
 
 export default router;
