ea486215b7956358192820a89133f9cbbc281b47
MoneyIgos <makowskiigor@gmail.com>
2021-02-08 10:48:50 +0100

    refactor(quiz): delete unused body validate

--- src/routes/quiz/index.ts
+++ src/routes/quiz/index.ts
@@ -5,21 +5,16 @@ import Playlist from '../../models/playlist.model';
 
 const router = Router();
 
-router.post(
-  '/quiz/:id',
-  body('accesToken').isString(),
-  authenticate,
-  async (req: Request, res: Response) => {
-    try {
-      const err = validationResult(req);
-      if (!err.isEmpty())
-        return res.status(400).send({ err: 'token is required' });
+router.post('/quiz/:id', authenticate, async (req: Request, res: Response) => {
+  try {
+    const err = validationResult(req);
+    if (!err.isEmpty())
+      return res.status(400).send({ err: 'token is required' });
 
-      const playlist = await Playlist.findOne({ _id: req.params.id });
-    } catch (err) {
-      res.status(500).send({ err });
-    }
+    const playlist = await Playlist.findOne({ _id: req.params.id });
+  } catch (err) {
+    res.status(500).send({ err });
   }
-);
+});
 
 export default router;
