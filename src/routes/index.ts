8ce3d216fd8bf8590a182deed07b708c087196f7
Kamil Pawlaczyk <prgmr.kamil.pawlaczyk@gmail.com>
2021-02-12 22:16:33 +0100

    feat: include playlist

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,12 @@
-import { Router } from 'express';
+import { Router, Request, Response } from 'express';
 import authRoutes from './auth';
 import adminRoutes from './admin';
+import playlistRoutes from './playlists';
 
 const router = Router();
 
 router.use('/auth', authRoutes);
 router.use('/admin', adminRoutes);
+router.use('/playlists', playlistRoutes);
 
 export default router;
