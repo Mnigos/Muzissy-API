85a781122f8989257a120038d0594ff27f697f36
Kamil Pawlaczyk <prgmr.kamil.pawlaczyk@gmail.com>
2021-02-12 22:16:11 +0100

    feat: create new endpoint

--- src/routes/playlists/index.ts
+++ src/routes/playlists/index.ts
@@ -1,9 +1,21 @@
 import { Request, Response, Router } from 'express';
-
+import Playlist from '../../models/playlist.model';
 const router = Router();
 
-router.get('/playlist', (req: Request, res: Response) => {
-  res.send('Playlists Here');
+router.get('/', (req: Request, res: Response) => {
+  const playlists = Playlist.find();
+  res.send(playlists);
+
+  Playlist.findOne((err: any, playlist: any) => {
+    if (err) {
+      return res.status(500).send({
+        err: 'Cannot get this from database'
+      });
+    }
+    res.status(200).send({
+      playlist
+    });
+  });
 });
 
 export default router;
