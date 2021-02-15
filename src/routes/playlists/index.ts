7336ff6f29a7f94f125eee56633ba02f3d5016a3
Agil-Dev <prgmr.kamil.pawlaczyk@gmail.com>
2021-02-15 13:52:00 +0100

    fix: @ changed to ../../

--- src/routes/playlists/index.ts
+++ src/routes/playlists/index.ts
@@ -1,5 +1,5 @@
 import { Request, Response, Router } from 'express';
-import Playlist from '@/models/playlist.model';
+import Playlist from '../../models/playlist.model';
 
 const router = Router();
 
