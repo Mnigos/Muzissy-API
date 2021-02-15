eff4a9d2fba3243dd1733d8451c2dfcb7efe6ec0
Agil-Dev <prgmr.kamil.pawlaczyk@gmail.com>
2021-02-15 13:42:39 +0100

    style: adaptation to ESlinter

--- src/routes/playlists/index.ts
+++ src/routes/playlists/index.ts
@@ -1,16 +1,17 @@
 import { Request, Response, Router } from 'express';
-import Playlist from '../../models/playlist.model';
+import Playlist from '@/models/playlist.model';
+
 const router = Router();
 
 router.post('/', (req: Request, res: Response) => {
   Playlist.find((err, playlist) => {
     if (err) {
       return res.status(500).send({
-        err: 'Cannot get this from database'
+        err: 'Cannot get this from database',
       });
     }
-    res.status(200).send({
-      playlist
+    return res.status(200).send({
+      playlist,
     });
   });
 });
