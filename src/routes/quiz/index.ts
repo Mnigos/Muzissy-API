9357c20034d9ed96ac4852ff15f2f3cc02ce9a0e
MoneyIgos <makowskiigor@gmail.com>
2021-02-26 10:36:51 +0100

    chore(quiz): add answer route to quiz router

--- src/routes/quiz/index.ts
+++ src/routes/quiz/index.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 import questionRoute from './question';
+import answerRoute from './answer';
 
 const router = Router();
 
 router.use('/question', questionRoute);
+router.use('/answer', answerRoute);
 
 export default router;
