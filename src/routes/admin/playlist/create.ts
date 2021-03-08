26df1dccebb647389954ada2576669d2780693eb
rysiek21 <rysiek21@users.noreply.github.com>
2021-03-08 13:23:08 +0100

    chore(playlist): delete unnecessary code

--- src/routes/admin/playlist/create.ts
+++ src/routes/admin/playlist/create.ts
@@ -1,5 +1,4 @@
 import { Request, Response, Router } from 'express';
-import { body, validationResult } from 'express-validator';
 import passport from 'passport';
 import Playlist from '../../../models/playlist.model';
 
@@ -7,28 +6,18 @@ const router = Router();
 
 router.post(
   '/create',
-  body('name').isString(),
-  body('img').isString(),
-  body('difficulty').isString(),
   passport.authenticate('bearer', { session: false }),
   async (req: Request, res: Response) => {
     try {
-      const err = validationResult(req);
-      if (!err.isEmpty())
-        return res
-          .status(400)
-          .send({ err: 'name image and difficulty are required in body' });
-      const { name, img, difficulty } = req.body;
+      const { playlist } = req.body;
 
-      const foundedName = await Playlist.findOne({ name });
+      const foundedName = await Playlist.findOne({ name: playlist.name });
 
       if (foundedName)
         return res.status(400).send({ err: 'playlistAlreadyExist' });
 
       new Playlist({
-        name,
-        img,
-        difficulty,
+        playlist,
       })
         .save()
         .then(() => {
