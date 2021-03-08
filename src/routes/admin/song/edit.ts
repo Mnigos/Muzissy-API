62e8a40c9198a27d3100a140bddc79303320725d
rysiek21 <rysiek21@users.noreply.github.com>
2021-03-08 13:10:53 +0100

    feat(song): add song endpoint

---
+++ src/routes/admin/song/edit.ts
@@ -0,0 +1,27 @@
+import { Request, Response, Router } from 'express';
+import passport from 'passport';
+import Song from '../../../models/song.model';
+
+const router = Router();
+
+router.post(
+  '/edit',
+  passport.authenticate('bearer', { session: false }),
+  async (req: Request, res: Response) => {
+    try {
+      const { song } = req.body;
+
+      let foundedSong = await Song.findOne({ id: song.id });
+
+      if (foundedSong) {
+        foundedSong = req.body.song;
+        foundedSong.save();
+        res.status(201).send({ message: 'Edited' });
+      } else return res.status(400).send({ err: 'songDoesNotExist' });
+    } catch (err) {
+      res.status(500).send({ err });
+    }
+  }
+);
+
+export default router;
